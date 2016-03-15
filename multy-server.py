ac584582de798fcf35a093af13f4ac9cf669cc8b
hariton594 <hariton_stas@mail.ru>
2016-03-15 15:18:17 +0300

    commit

--- multy-server.py
+++ multy-server.py
@@ -1,7 +1,9 @@
 import socket                                                                  
-s = socket.socket()                                                            
-s.bind(('', 2222))                                                              
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                            
+s.bind(('0.0.0.0', 2222))                                                              
 s.listen(10)                                                                    
+
+while True:
 conn, addr = s.accept()                                                        
 while True:                                                                    
                 data = conn.recv(1024)                                          
