6082b641046d2851a109a56dc436adaea682502b
hariton594 <hariton_stas@mail.ru>
2016-03-15 15:41:30 +0300

    commit

--- multy-server.py
+++ multy-server.py
@@ -1,5 +1,6 @@
 import socket                                                                  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                            
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('0.0.0.0', 2222))                                                              
 s.listen(10)                                                                    
 print('start listening')
