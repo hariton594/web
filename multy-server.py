e8910cafb8118d045d06246c72c5816ac59fe477
hariton594 <hariton_stas@mail.ru>
2016-03-15 15:21:29 +0300

    commit

--- multy-server.py
+++ multy-server.py
@@ -4,9 +4,9 @@ s.bind(('0.0.0.0', 2222))
 s.listen(10)                                                                    
 
 while True:
-conn, addr = s.accept()                                                        
-while True:                                                                    
+	conn, addr = s.accept()                                                        
+	while True:                                                                    
                 data = conn.recv(1024)                                          
                 if data=='close' : break                                        
                 conn.send(data)                                                
-conn.close()
+	conn.close()
