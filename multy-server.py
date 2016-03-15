cc63016fd1c57eefbe1ec411ff17de254f276802
hariton594 <hariton_stas@mail.ru>
2016-03-15 15:34:56 +0300

    commit

--- multy-server.py
+++ multy-server.py
@@ -2,11 +2,13 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                                                            
 s.bind(('0.0.0.0', 2222))                                                              
 s.listen(10)                                                                    
-
+print('start listening')
 while True:
-	conn, addr = s.accept()                                                        
+	conn, addr = s.accept()
+	print('new connection', addr)                                                        
 	while True:                                                                    
                 data = conn.recv(1024)                                          
-                if data=='close' : break                                        
-                conn.send(data)                                                
+                if data=='close' :
+			print('get close')                                         
+                	conn.send(data)                                                
 	conn.close()
