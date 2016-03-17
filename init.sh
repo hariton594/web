30a6a9b3f40b0429d3e35d43394f426f36475218
hariton594 <hariton_stas@mail.ru>
2016-03-18 01:37:09 +0300

    commit

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/etc/gunicorn.config   /etc/gunicorn.d/django
 #sudo /etc/init.d/gunicorn restart
